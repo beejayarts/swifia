c804d90fd0c1990817e79785b9a55b23684d8e81
Beejay <onuohabenjamin33@gmail.com>
2023-02-16 21:14:35 +0100

    first commit

---
+++ scripts/dashboardPages.js
@@ -0,0 +1,11 @@
+// HTML Template for all the dashboard pages
+
+export const pages = {
+  dashboard:
+    "<h1 style='color:red'>Dashboard</h1><p>This is the dashboard page.</p>",
+  accounts: "<h1>ACCOUNTS</h1><p>This is the settings page.</p>",
+  transactions: "<h1>transactions</h1><p>This is the reports page.</p>",
+  profile: "<h1>profile</h1><p>This is the reports page.</p>",
+  support: "<h1>support</h1><p>This is the reports page.</p>",
+  logout: "<h1>Logout</h1><p>This is the reports page.</p>",
+};
